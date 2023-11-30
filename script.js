0bdf33bd6dc31f23e438772c4400b76ac32618c8
Rakesh Patel <107678438+RAKESH-PATEL57@users.noreply.github.com>
2023-11-30 10:48:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+let userInput = document.querySelector("#user-input");
+let addBtn = document.querySelector('.btn');
+let userContents = document.querySelector('.list');
+let searchSec = document.querySelector("#form");
+let clrBtn = document.querySelector("#clear");
+
+addBtn.onclick = () =>{
+    let userValue = userInput.value;
+    
+    if(userValue == '')
+    {
+        alert('Please enter your content');
+    }
+
+    else
+    {
+        searchSec.style.top="10%";
+        const box = document.createElement("li");
+        box.id = '#newContent';
+        box.href = `#${userValue}`;
+        box.innerText = userValue;
+        userContents.appendChild(box);
+    }
+}
+
+clrBtn.onclick = () => {
+    userContents.innerHTML = '';
+    // alert('click');
+} 
+
+
 
