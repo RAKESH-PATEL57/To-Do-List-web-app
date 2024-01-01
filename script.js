b60c5251afad9121dc890223d0d8a782df09dd4e
Rakesh Patel <107678438+RAKESH-PATEL57@users.noreply.github.com>
2024-01-01 10:31:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let userInput = document.querySelector("#user-input");
 let addBtn = document.querySelector('.btn');
 let userContents = document.querySelector('.list');
-let searchSec = document.querySelector("#form");
 let clrBtn = document.querySelector("#clear");
+let ch = document.querySelector("#check");
 
 addBtn.onclick = () =>{
     let userValue = userInput.value;
@@ -14,19 +14,53 @@ addBtn.onclick = () =>{
 
     else
     {
-        searchSec.style.top="10%";
-        const box = document.createElement("li");
-        box.id = '#newContent';
-        box.href = `#${userValue}`;
-        box.innerText = userValue;
+        let box = document.createElement("li");
+        // box.id = '#newContent';
+        // box.href = `#${userValue}`;
+        box.innerHTML = userValue;
         userContents.appendChild(box);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\xd7";
+        box.appendChild(span);
     }
+
+    userValue = "";
+    saveData();
+    
 }
+
+
 
 clrBtn.onclick = () => {
     userContents.innerHTML = '';
     // alert('click');
 } 
+
+userContents.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data",userContents.innerHTML);
+    console.log("rakesh");
+}
+
+
+function showTask(){
+    userContents.innerHTML = localStorage.getItem("data");
+}
+
+
+// saveData();
+showTask();
 
 
 
